75690e4c7615e0c30145694b62e7f5694dd68950
Dave Goldstein <dpg3k@virginia.edu>
2020-11-02 12:18:54 -0500

    Make the /healthcheck endpoint for standard

--- viewsrv/healthcheck.go
+++ viewsrv/healthcheck.go
@@ -9,24 +9,37 @@ import (
 )
 
 func healthCheckHandler(c *gin.Context) {
+
+	type healthcheck struct {
+		Healthy bool   `json:"healthy"`
+		Message string `json:"message"`
+	}
+
 	log.Printf("INFO: checking Tracksys...")
 	url := fmt.Sprintf("%s/pid/uva-lib:1157560/type", config.tracksysURL)
 
-	tsStatus := true
-	_, err := getAPIResponse( url )
+	tsStatus := healthcheck{true, ""}
+	_, err := getAPIResponse(url)
 	if err != nil {
-		tsStatus = false
+		tsStatus.Healthy = false
+		tsStatus.Message = err.Error()
 	}
 
 	// make sure IIIF manifest service is alive
 	log.Printf("INFO: checking IIIF...")
-	url = fmt.Sprintf( "%s/version", config.iiifURL)
-	iiifStatus := true
+	url = fmt.Sprintf("%s/version", config.iiifURL)
+	iiifStatus := healthcheck{true, ""}
 
-	_, err = getAPIResponse( url )
+	_, err = getAPIResponse(url)
 	if err != nil {
-		iiifStatus = false
+		iiifStatus.Healthy = false
+		iiifStatus.Message = err.Error()
 	}
 
-	c.JSON(http.StatusOK, gin.H{"alive": true, "iiif": iiifStatus, "tracksys": tsStatus})
+	httpStatus := http.StatusOK
+	if tsStatus.Healthy == false || iiifStatus.Healthy == false {
+		httpStatus = http.StatusInternalServerError
+	}
+
+	c.JSON(httpStatus, gin.H{"tracksys": tsStatus, "iiifmanifest": iiifStatus})
 }
