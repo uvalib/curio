fe6b08635bd28839e5cf3b30bcc464f687bbe8ac
Dave Goldstein <dpg3k@virginia.edu>
2021-02-25 10:46:44 -0500

    VIRGONEW-2098: disable healthchecking of tracksys

--- viewsrv/healthcheck.go
+++ viewsrv/healthcheck.go
@@ -15,31 +15,33 @@ func healthCheckHandler(c *gin.Context) {
 		Message string `json:"message"`
 	}
 
-	log.Printf("INFO: checking Tracksys...")
-	url := fmt.Sprintf("%s/pid/uva-lib:1157560/type", config.tracksysURL)
+	//log.Printf("INFO: checking Tracksys...")
+	//url := fmt.Sprintf("%s/pid/uva-lib:1157560/type", config.tracksysURL)
 
-	tsStatus := healthcheck{true, ""}
-	_, err := getAPIResponse(url)
-	if err != nil {
-		tsStatus.Healthy = false
-		tsStatus.Message = err.Error()
-	}
+	//tsStatus := healthcheck{true, ""}
+	//_, err := getAPIResponse(url)
+	//if err != nil {
+	//	tsStatus.Healthy = false
+	//	tsStatus.Message = err.Error()
+	//}
 
 	// make sure IIIF manifest service is alive
 	log.Printf("INFO: checking IIIF...")
-	url = fmt.Sprintf("%s/version", config.iiifURL)
+	url := fmt.Sprintf("%s/version", config.iiifURL)
 	iiifStatus := healthcheck{true, ""}
 
-	_, err = getAPIResponse(url)
+	_, err := getAPIResponse(url)
 	if err != nil {
 		iiifStatus.Healthy = false
 		iiifStatus.Message = err.Error()
 	}
 
 	httpStatus := http.StatusOK
-	if tsStatus.Healthy == false || iiifStatus.Healthy == false {
+//	if tsStatus.Healthy == false || iiifStatus.Healthy == false {
+	if iiifStatus.Healthy == false {
 		httpStatus = http.StatusInternalServerError
 	}
 
-	c.JSON(httpStatus, gin.H{"tracksys": tsStatus, "iiifmanifest": iiifStatus})
+//	c.JSON(httpStatus, gin.H{"tracksys": tsStatus, "iiifmanifest": iiifStatus})
+	c.JSON(httpStatus, gin.H{"iiifmanifest": iiifStatus})
 }
