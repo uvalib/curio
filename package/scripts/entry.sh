e42f23665e52e69a3ac9d36986c9b9f3e7a2185f
Lou Foster <lf6f@virginia.edu>
2021-04-02 10:26:21 -0400

    remove -fedora param

--- package/scripts/entry.sh
+++ package/scripts/entry.sh
@@ -1,7 +1,7 @@
 # run application
 
 # run from here, since application expects web template in web/
-cd bin; ./curio -apollo $APOLLO_URL -iiif $CURIO_IIIF_MAN_URL -iiifcache $CURIO_IIIF_CACHE_URL -fedora $WSLS_FEDORA_URL -rights $RIGHTS_WRAPPER_URL -host $CURIO_HOST
+cd bin; ./curio -apollo $APOLLO_URL -iiif $CURIO_IIIF_MAN_URL -iiifcache $CURIO_IIIF_CACHE_URL -rights $RIGHTS_WRAPPER_URL -host $CURIO_HOST
 
 #
 # end of file
