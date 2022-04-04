32403751d6f3e960ee889c5d3b422902503adb8c
Lou Foster <louffoster@gmail.com>
2022-04-04 10:41:59 -0400

    convert to vite

---
+++ frontend/vite.config.js
@@ -0,0 +1,44 @@
+ /*global process */
+
+import { fileURLToPath, URL } from 'url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+   plugins: [vue()],
+   resolve: {
+      alias: {
+         '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+   },
+   server: { // this is used in dev mode only
+      port: 8080,
+      proxy: {
+         '/api': {
+            target: process.env.CURIO_SRV, // export V4_CONFIG=http://localhost:8095
+            changeOrigin: true,
+            logLevel: 'debug'
+          },
+          '/healthcheck': {
+            target: process.env.V4_CONFIG,
+            changeOrigin: true,
+            logLevel: 'debug'
+          },
+          '/version': {
+            target: process.env.V4_CONFIG,
+            changeOrigin: true,
+            logLevel: 'debug'
+          },
+      }
+   },
+   //  configureWebpack: {
+   //    performance: {
+   //      // bump max sizes to 1024
+   //      maxEntrypointSize: 1024000,
+   //      maxAssetSize: 1024000
+   //    }
+   //  },
+})
+
+
